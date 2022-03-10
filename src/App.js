a3788359bb99f1b0f04d4874a04242a2ab715b7c
Sam Courtenay <sam.m.courtenay@gmail.com>
2022-03-10 15:23:02 -0800

    working on finishing the userLogin feature with jwt-decode

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 import NavBar from "./NavBar";
 import Routes from "./Routes";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import JoblyApi from "./api";
+import jwt_decode from "jwt-decode";
 
 /**
  *  Renders NavBar and Routes
@@ -14,14 +15,26 @@ import JoblyApi from "./api";
  */
 function App() {
   console.log("Entering App Component");
+  const [token, setToken] = useState("");
   const [user, setUser] = useState({});
+
+  const payload = jwt_decode(token);
+  console.log(payload);
+  
+  useEffect(function XYZ(){
+    async function ABC(){
+      const payload = jwt_decode(token);
+      console.log(payload);
+      // const userLoggedIn = await JoblyApi.getUserByUsername(formData.username);
+      // setUser(userLoggedIn);
+    }
+    ABC();
+  }, [token]);
+
 
   async function handleLogin(formData) {
     const token = await JoblyApi.login(formData);
-    if (token) {
-      const userLoggedIn = await JoblyApi.getUserByUsername(formData.username);
-      setUser(userLoggedIn);
-    }
+    setToken(() => token)
   }
 
   return (
